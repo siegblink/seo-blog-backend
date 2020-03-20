0fabdb36bd7533cea59c1490ac13d15269d60a8a
Sieg <siegblink@gmail.com>
2020-03-20 15:30:03 +0800

    Update .env file. Add routes directory and update server.js file.

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+// Bring in routes
+const blogRoutes = require('./routes/blog')
 
 // App
 const app = express()
 
 // Database
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_CLOUD, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
@@ -29,10 +31,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
 }
 
-// Routes
-app.get('/api', function(res, res) {
-  res.json({ time: Date().toString() })
-})
+// Routes middleware
+app.use('/api', blogRoutes)
 
 // Port
 const port = process.env.PORT || 8000

d266472ffceb3e55294da39a2402845efb23316c
Sieg <siegblink@gmail.com>
2020-03-21 19:03:46 +0800

    Add signin, signout, and secret routes. Add signin and signout middleware.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user')
 const shortId = require('shortid')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 
 exports.signup = function(req, res) {
   User.findOne({ email: req.body.email }).exec(function(err, user) {
@@ -23,3 +25,43 @@ exports.signup = function(req, res) {
     })
   })
 }
+
+exports.signin = function(req, res) {
+  const { email, password } = req.body
+  // Check if user exist
+  User.findOne({ email }).exec(function(err, userData) {
+    if (err || !userData) {
+      return res.status(400).json({
+        error: 'User with that email does not exist. Please sign up.',
+      })
+    }
+    // Authenticate
+    if (!userData.authenticate(password)) {
+      return status(400).json({
+        error: 'Email and password do not match.',
+      })
+    }
+    // Generate a token and send to client
+    const token = jwt.sign({ _id: userData._id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    })
+
+    res.cookie('token', token, { expiresIn: '1d' })
+    const { _id, username, name, email, role } = userData
+    return res.json({
+      token,
+      user: { _id, username, name, email, role },
+    })
+  })
+}
+
+exports.signout = function(req, res) {
+  res.clearCookie('token')
+  res.json({
+    message: 'Sign out success.',
+  })
+}
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+})
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const { signup } = require('../controllers/auth')
+const { signup, signin, signout } = require('../controllers/auth')
+const { requireSignin } = require('../controllers/auth')
 
 // Validators
 const { runValidation } = require('../validators')
 const { userSignupValidator } = require('../validators/auth')
+const { userSigninValidator } = require('../validators/auth')
 
 router.post('/signup', userSignupValidator, runValidation, signup)
+router.post('/signin', userSigninValidator, runValidation, signin)
+router.get('/signout', signout)
+// Test
+router.get('/secret', requireSignin, function(req, res) {
+  res.json({
+    message: 'You have access to secret page.',
+  })
+})
 
 module.exports = router
